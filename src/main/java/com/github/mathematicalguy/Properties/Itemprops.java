6f6f081f232ef68dfec7293d3da94d8d4f675f40
Poetker <poetker_g21@moeller.org>
2020-03-05 14:48:44 -0500

    reorganizin trying to make block drop
    reoprganizing

---
+++ src/main/java/com/github/mathematicalguy/Properties/Itemprops.java
@@ -0,0 +1,8 @@
+package com.github.mathematicalguy.Properties;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class Itemprops {
+   public static Item.Properties CopperProperties = new Item.Properties().group(ItemGroup.BUILDING_BLOCKS);  // which inventory tab?
+}
--- src/main/java/com/github/mathematicalguy/blocks/BlockBasic.java
+++ src/main/java/com/github/mathematicalguy/blocks/BlockBasic.java
@@ -2,14 +2,40 @@ package com.github.mathematicalguy.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
 
+import java.util.Random;
 
 public class BlockBasic extends Block {
-    public BlockBasic()
+    private Item drop;
+    private int meta;
+    private int least_quantity;
+    private int most_quantity;
+        public BlockBasic(){
+        super(Block.Properties.create(Material.IRON).hardnessAndResistance(6));
+    }
+    public BlockBasic(BlockItem drop, int meta, int least_quantity, int most_quantity)
     {
-        super(Block.Properties.create(Material.IRON).hardnessAndResistance(6) // look at Block.Properties for further options
-                // typically useful: hardnessAndResistance(), harvestLevel(), harvestTool()
-        );
+        super(Block.Properties.create(Material.IRON).hardnessAndResistance(6));
+        this.drop = drop;
+        this.meta = meta;
+        this.least_quantity = least_quantity;
+        this.most_quantity = most_quantity;
     }
 
+    public Item getItemDropped(int meta, Random random, int fortune) {
+        return this.drop;
+    }
+
+
+    public int damageDropped(int metadata) {
+        return this.meta;
+    }
+
+    public int quantityDropped(int meta, int fortune, Random random) {
+        if (this.least_quantity >= this.most_quantity)
+            return this.least_quantity;
+        return this.least_quantity + random.nextInt(this.most_quantity - this.least_quantity + fortune + 1);
+    }
 }
