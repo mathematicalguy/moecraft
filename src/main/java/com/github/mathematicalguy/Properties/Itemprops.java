8182d783384ef2aa117a344fc640bc83632811f0
Poetker <poetker_g21@moeller.org>
2020-03-10 13:39:25 -0400

    Tooltype
    Removed hoe added Tooltype

--- src/main/java/com/github/mathematicalguy/Properties/Itemprops.java
+++ src/main/java/com/github/mathematicalguy/Properties/Itemprops.java
@@ -2,9 +2,12 @@ package com.github.mathematicalguy.Properties;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
 
 public class Itemprops {
    public static Item.Properties OreProperties = new Item.Properties().group(ItemGroup.BUILDING_BLOCKS);  // which inventory tab?
    public static Item.Properties IngotProperties = new Item.Properties().group(ItemGroup.MATERIALS);
-   public static Item.Properties tool = new Item.Properties().group(ItemGroup.TOOLS).maxStackSize(1);
+   public static Item.Properties StonePickaxe = new Item.Properties().group(ItemGroup.TOOLS).maxStackSize(1).addToolType(ToolType.PICKAXE, 2);
+   public static Item.Properties StoneAxe = new Item.Properties().group(ItemGroup.TOOLS).maxStackSize(1).addToolType(ToolType.AXE, 2);
+   public static Item.Properties StoneShovel = new Item.Properties().group(ItemGroup.TOOLS).maxStackSize(1).addToolType(ToolType.SHOVEL, 2);
 }
--- src/main/java/com/github/mathematicalguy/init/ModItems.java
+++ src/main/java/com/github/mathematicalguy/init/ModItems.java
@@ -8,9 +8,8 @@ public class ModItems {
     public static Item CopperIngot =new Item(Itemprops.IngotProperties).setRegistryName("copper_ingot");
     public static Item aluminumIngot = new Item(Itemprops.IngotProperties).setRegistryName("aluminum_ingot");
 
-    public static Item CopperPickaxe = new PickaxeItem(ItemTeirs.aluminumcopper, 3, 2, Itemprops.tool).setRegistryName("copper_pickaxe");
-    public static Item CopperAxe = new AxeItem(ItemTeirs.aluminumcopper, 4,1,Itemprops.tool).setRegistryName("copper_axe");
-    public static Item CopperHoe = new HoeItem(ItemTeirs.aluminumcopper, 2, Itemprops.tool).setRegistryName("copper_hoe");
-    public static Item CopperShovel = new ShovelItem(ItemTeirs.aluminumcopper, 2, 2, Itemprops.tool).setRegistryName("copper_shovel");
-    public static Item CopperSword = new SwordItem(ItemTeirs.aluminumcopper, 5, 3, Itemprops.tool).setRegistryName("copper_sword");
+    public static Item CopperPickaxe = new PickaxeItem(ItemTeirs.aluminumcopper, 2, 2, Itemprops.StonePickaxe).setRegistryName("copper_pickaxe");
+    public static Item CopperAxe = new AxeItem(ItemTeirs.aluminumcopper, 4,1,Itemprops.StoneAxe).setRegistryName("copper_axe");
+    public static Item CopperShovel = new ShovelItem(ItemTeirs.aluminumcopper, 1, 2, Itemprops.StoneShovel).setRegistryName("copper_shovel");
+    public static Item CopperSword = new SwordItem(ItemTeirs.aluminumcopper, 5, 3, Itemprops.StoneAxe).setRegistryName("copper_sword");
 }
