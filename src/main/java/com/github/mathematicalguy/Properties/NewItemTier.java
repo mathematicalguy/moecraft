28203817c05e5d66023d1bf5e7ae54bfab6335ae
Poetker <poetker_g21@moeller.org>
2020-03-12 11:45:29 -0400

    WIP UNTESTED
    created an item teir for aluminum and copper in a new enum

---
+++ src/main/java/com/github/mathematicalguy/Properties/NewItemTier.java
@@ -0,0 +1,56 @@
+package com.github.mathematicalguy.Properties;
+
+import com.github.mathematicalguy.init.ModItems;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
+
+import java.util.function.Supplier;
+
+public enum NewItemTier implements IItemTier {
+    ALUMINUM(1, 100, 4.0F, 1.0F, 5, () -> {
+        return Ingredient.fromItems(ModItems.aluminumIngot);
+    }),
+    COPPER(1, 200, 4.0F, 1.0F, 5, () -> {
+        return Ingredient.fromItems(ModItems.CopperIngot);
+    });
+    private final int harvestLevel;
+    private final int maxUses;
+    private final float efficiency;
+    private final float attackDamage;
+    private final int enchantability;
+    private final LazyValue<Ingredient> repairMaterial;
+
+    private NewItemTier(int harvestLevelIn, int maxUsesIn, float efficiencyIn, float attackDamageIn, int enchantabilityIn, Supplier<Ingredient> repairMaterialIn) {
+        this.harvestLevel = harvestLevelIn;
+        this.maxUses = maxUsesIn;
+        this.efficiency = efficiencyIn;
+        this.attackDamage = attackDamageIn;
+        this.enchantability = enchantabilityIn;
+        this.repairMaterial = new LazyValue<>(repairMaterialIn);
+    }
+    public int getMaxUses() {
+        return this.maxUses;
+    }
+
+    public float getEfficiency() {
+        return this.efficiency;
+    }
+
+    public float getAttackDamage() {
+        return this.attackDamage;
+    }
+
+    public int getHarvestLevel() {
+        return this.harvestLevel;
+    }
+
+    public int getEnchantability() {
+        return this.enchantability;
+    }
+
+    public Ingredient getRepairMaterial() {
+        return this.repairMaterial.getValue();
+    }
+}
