a773b73111c5e704b8876e720efcae1c7a643374
Garrett Poetker <61630108+mathematicalguy@users.noreply.github.com>
2020-08-14 15:23:30 -0400

    just changed how it identifys modid

--- src/main/java/com/github/mathematicalguy/MoeCraftMod.java
+++ src/main/java/com/github/mathematicalguy/MoeCraftMod.java
@@ -29,7 +29,7 @@ import java.util.stream.Collectors;
 import static com.github.mathematicalguy.MoeCraftMod.MOD_ID;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(MOD_ID)
+@Mod(MoeCraftMod.MOD_ID)
 public class MoeCraftMod
 {
     public static final String MOD_ID = "moecraft";
